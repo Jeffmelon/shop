87c1b8d7600359c8ee024c882725975505a575cf
Jeffmelon <megamanethan@gmail.com>
2023-11-01 20:33:49 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Nav from './Pages/Nav';
 import Home from './Pages/Home';
 import Sticker from './Pages/Sticker';
 import Poster from './Pages/Poster';
@@ -10,14 +11,13 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shirt" element={<Shirt />} />
           <Route path="/sticker" element={<Sticker />} />
           <Route path="/poster" element={<Poster />} />
         </Routes>
-        <Footer />
       </Router>
     </div>
   );
---
+++ src/Pages/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import "../css/nav.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
+
+function Nav() {
+    const [expandNav, setExpand] = useState(false);
+
+    const location = useLocation();
+  
+    useEffect(() => {
+      setExpand(false);
+    }, [location]);
+  
+    return (
+      <div className='navBar' id={expandNav ? "open" : "close"}>
+          <div className='toggle'>
+              <button 
+                onClick={() => {
+                  setExpand((prev) => !prev)
+                }}
+              >
+                <ReorderIcon />
+              </button>
+          </div>
+          <div className='links'>
+              <Link to="/"> Home </Link>
+              <Link to="/shirt"> Shirt </Link>
+              <Link to="/sticker"> Sticker </Link>
+              <Link to="/poster"> Poster </Link>
+          </div>
+      </div>
+    )
+}
+
+export default Nav
