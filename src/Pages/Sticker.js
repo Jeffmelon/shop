2d9d273cfca5465ef3a64ea80bc3a8aa160900e5
Jeffmelon <megamanethan@gmail.com>
2023-11-16 11:31:28 -0700

    deleted

--- src/Pages/Sticker.js
+++ src/Pages/Sticker.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StickerList } from '../Component/StickerList'
 import StickerDisplay from '../Display/StickerDisplay'
-
+import "../Style/Sticker.css"
 function Sticker() {
   return (
-    <div>
+    <div className='shop'>
       <h1>Cool Stickers</h1>
-      <div>
+      <div className='shopList'>
         {StickerList.map((ShopSticker) => {
           return <StickerDisplay img={ShopSticker.Image} name={ShopSticker.name} link={ShopSticker.link} price={ShopSticker.price}/>
         })}
