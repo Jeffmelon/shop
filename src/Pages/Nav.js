a9fa8ca191007261fad63be0acae73847637a4ad
Jeffmelon <megamanethan@gmail.com>
2023-11-18 19:49:55 -0700

    n

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import "../css/nav.css";
+import "../Style/nav.css";
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Nav() {
