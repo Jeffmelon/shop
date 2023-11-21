f28fb6f94c09f1d881c9e6e3246049941c56dd59
Jeffmelon <megamanethan@gmail.com>
2023-11-21 16:28:56 -0700

    s

--- src/Display/StickerDisplay.js
+++ src/Display/StickerDisplay.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../Style/Sticker.css"
 
 function StickerDisplay({img, name, link, price}) {
     return (
         <div onClick={() => window.open(link, "_blank")}>
-            <div style={{ backgroundImage: `url(${img})`}}/>
+            <div style={{ backgroundImage: {img}}}/>
             <h1>{name}</h1>
             <p>{price}</p>
         </div>
