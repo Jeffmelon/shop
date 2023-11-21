dd0d35d3b67526a52a4a12524dc80793e608b58a
Jeffmelon <megamanethan@gmail.com>
2023-11-21 16:22:16 -0700

    STICKERS

--- src/Component/StickerList.js
+++ src/Component/StickerList.js
@@ -1,126 +1,148 @@
+import CY from "../Items/CY.jpg"
+import D from "../Items/D.jpg"
+import DS from "../Items/DS.jpg"
+import FM from "../Items/FM.jpg"
+import GA from "../Items/GA.jpg"
+import GC from "../Items/GC.jpg"
+import GH from "../Items/GH.jpg"
+import HP from "../Items/HP.jpg"
+import JD from "../Items/JD.jpg"
+import M from "../Items/M.jpg"
+import MD from "../Items/MD.jpg"
+import S from "../Items/S.jpg"
+import SK from "../Items/SK.jpg"
+import SS from "../Items/SS.jpg"
+import SD from "../Items/SD.jpg"
+import TC from "../Items/TC.jpg"
+import V from "../Items/V.jpg"
+import W from "../Items/W.jpg"
+import WB from "../Items/WB.jpg"
+import WC from "../Items/WC.jpg"
+import Z from "../Items/Z.jpg"
+
 export const StickerList = [
     {
-        Image: placeholder,
+        Image: GC,
         name: "Ghostly Cow",
         link: "https://www.redbubble.com/i/sticker/Ghostly-Cow-by-Fall-Lover/153643838.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: TC,
         name: "Thirsty Chupacabra",
         link: "https://www.redbubble.com/i/sticker/Thirsty-Chupacabra-by-Fall-Lover/154371532.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: D,
         name: "Demon",
         link: "https://www.redbubble.com/i/sticker/Demon-by-Fall-Lover/154370000.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: Z,
         name: "Zossum",
         link: "https://www.redbubble.com/i/sticker/Zossum-by-Fall-Lover/153643710.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: V,
         name: "Vampire",
         link: "https://www.redbubble.com/i/sticker/Vampire-by-Fall-Lover/154370157.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: GH,
         name: "Ghost Hunting",
         link: "https://www.redbubble.com/i/sticker/Ghost-Hunting-by-Fall-Lover/152974170.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: WC,
         name: "Witchy Cat",
         link: "https://www.redbubble.com/i/sticker/Witchy-Cat-by-Fall-Lover/154370243.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: SS,
         name: "Sweet & Slimy",
         link: "https://www.redbubble.com/i/sticker/Sweet-and-Slimy-by-Fall-Lover/153644085.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: MD,
         name: "Monster of the Deep",
         link: "https://www.redbubble.com/i/sticker/Fish-by-Fall-Lover/153647881.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: S,
         name: "Scarecrow",
         link: "https://www.redbubble.com/i/sticker/Scarecrow-by-Fall-Lover/154371593.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: DS,
         name: "Deer Skeleton",
         link: "https://www.redbubble.com/i/sticker/Deer-Skeleton-by-Fall-Lover/153643797.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: FM,
         name: "Flatwoods Monster",
         link: "https://www.redbubble.com/i/sticker/Flatwoods-Monster-by-Fall-Lover/154372012.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: M,
         name: "Moth",
         link: "https://www.redbubble.com/i/sticker/Mothman-by-Fall-Lover/154371651.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: CY,
         name: "Cozy Yeti",
         link: "https://www.redbubble.com/i/sticker/Warm-Yeti-by-Fall-Lover/154371481.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: JD,
         name: "Jersey Devil",
         link: "https://www.redbubble.com/i/sticker/Jersey-Devil-by-Fall-Lover/154370855.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: SD,
         name: "Sweet Dragon",
         link: "https://www.redbubble.com/i/sticker/Sweet-Dragon-by-Fall-Lover/154370439.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: HP,
         name: "Hocus & Pocus",
         link: "https://www.redbubble.com/i/sticker/Hocus-and-Pocus-by-Fall-Lover/154370344.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: W,
         name: "Werewolf",
         link: "https://www.redbubble.com/i/sticker/Werewolf-by-Fall-Lover/154370054.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: WB,
         name: "Witchy Business",
         link: "https://www.redbubble.com/i/sticker/Witchy-Business-by-Fall-Lover/154369962.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: GA,
         name: "Ghostly Alien",
         link: "https://www.redbubble.com/i/sticker/Ghostly-Alien-by-Fall-Lover/154369902.EJUG5",
         price: "$2.69",
     },
     {
-        Image: placeholder,
+        Image: SK,
         name: "Sleepy Fall Kitty",
         link: "https://www.redbubble.com/i/sticker/Sleepy-kitty-by-Fall-Lover/152969762.EJUG5",
         price: "$2.69",
