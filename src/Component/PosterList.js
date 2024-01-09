4d9149e2a381b80c2d718e9feb219d1ae6d633db
Jeffmelon <megamanethan@gmail.com>
2024-01-09 10:53:54 -0700

    poster

--- src/Component/PosterList.js
+++ src/Component/PosterList.js
@@ -12,6 +12,16 @@ export const PosterList = [
     {
         Image: placeholder,
         name: "Moonlight Madness",
+        link: "https://www.redbubble.com/i/poster/Moonlight-Madness-by-Fall-Lover/150592141.LVTDI",
+    },
+    {
+        Image: placeholder,
+        name: "Ghostly Alien",
         link: placeholder,
-    }
+    },
+    {
+        Image: placeholder,
+        name: "Fall Walk",
+        link: placeholder,
+    },
 ]
