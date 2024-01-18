ce435de834e24d2d2a1ef73b49aae03925e4dd26
Jeffmelon <megamanethan@gmail.com>
2024-01-18 11:03:46 -0700

    Posters

--- src/Component/PosterList.js
+++ src/Component/PosterList.js
@@ -1,27 +1,38 @@
+import GH from "../Items/GHposter.jpg"
+import FW from "../Items/FWposter.jpg"
+import GA from "../Items/GAposter.jpg"
+import MM from "../Items/MMposter.jpg"
+import WB from "../Items/WBposter.jpg"
+
 export const PosterList = [
     {
-        Image: placeholder,
+        Image: GH,
         name: "Ghost Hunter",
         link: "https://www.redbubble.com/i/photographic-print/Ghost-Hunting-by-Fall-Lover/152974170.6Q0TX",
+        price: "$24.04"
     },
     {
-        Image: placeholder,
+        Image: FW,
         name: "Fall Walk",
         link: "https://www.redbubble.com/i/art-print/Fall-walk-by-Fall-Lover/152962527.1G4ZT",
+        price: "$24.04"
     },
     {
-        Image: placeholder,
+        Image: MM,
         name: "Moonlight Madness",
         link: "https://www.redbubble.com/i/poster/Moonlight-Madness-by-Fall-Lover/150592141.LVTDI",
+        price: "$28.84"
     },
     {
-        Image: placeholder,
+        Image: GA,
         name: "Ghostly Alien",
         link: "https://www.redbubble.com/i/art-print/Ghostly-Alien-by-Fall-Lover/154369902.1G4ZT",
+        price: "$24.04"
     },
     {
-        Image: placeholder,
+        Image: FW,
         name: "Fall Walk",
         link: "https://www.redbubble.com/i/art-print/Witchy-Business-by-Fall-Lover/154369962.1G4ZT",
+        price: "$24.04"
     },
 ]
--- src/Component/ShirtList.js
+++ src/Component/ShirtList.js
@@ -8,20 +8,24 @@ export const ShirtList = [
         Image: PUshirt,
         name: "Playing Un-Dead",
         link: "https://www.redbubble.com/i/t-shirt/Playing-Undead-by-Fall-Lover/151251313.22W0K.XYZ",
+        price: "$23.34"
     },
     {
         Image: kittyshirt,
         name: "Sleepy Kitty",
         link: "https://www.redbubble.com/i/t-shirt/Sleepy-kitty-by-Fall-Lover/152969762.WFLAH.XYZ",
+        price: "$23.34"
     },
     {
         Image: moonlightshirt,
         name: "Moonlight Maddness",
         link: "https://www.redbubble.com/i/t-shirt/Moonlight-Madness-by-Fall-Lover/150592141.WFLAH.XYZ",
+        price: "$23.34"
     },
     {
         Image: bigyetishirt,
         name: "The Biggest Yeti!",
         link: "https://www.redbubble.com/i/t-shirt/The-Biggest-Yeti-by-Fall-Lover/155648901.240QL",
+        price: "$23.34"
     }
 ]
